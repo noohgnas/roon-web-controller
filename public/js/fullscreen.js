a09e9849ccb25d0ef427c7dd80d5c79d00624d5c
Sanghoon <noohgnas@gmail.com>
2017-08-24 12:44:20 +0900

    add quick link

--- public/js/fullscreen.js
+++ public/js/fullscreen.js
@@ -7,6 +7,7 @@ var socket = io();
 
 $(document).ready(function() {
     $("#buttonMenu").html(getSVG('menu'));
+    $("#quickLink").html(getSVG('libraryBrowser'));
 
     socket.on("pairStatus", function(payload) {
         var pairEnabled = payload.pairEnabled;
@@ -19,6 +20,25 @@ $(document).ready(function() {
     });
 
 });
+
+function moveToLink(o) {
+    var target = o.attr("data-move-to");
+
+    showSection(target);
+
+    switch (target) {
+        case "nowPlaying":
+            $("#quickLink").html(getSVG('libraryBrowser'));
+            o.attr("data-move-to", "libraryBrowser");
+            break;
+        case "libraryBrowser":
+            $("#quickLink").html(getSVG('nowPlaying'));
+            o.attr("data-move-to", "nowPlaying");
+            break;
+        default:
+            break;
+        }
+}
 
 function showSection(sectionName) {
     switch (sectionName) {
@@ -75,6 +95,10 @@ function getSVG(cmd) {
     switch (cmd) {
         case "menu":
             return "<svg viewBox=\"0 0 512 512\"><path d=\"M64 128h384v42.667H64V128m0 106.667h384v42.666H64v-42.666m0 106.666h384V384H64v-42.667z\"/></svg>";
+        case "libraryBrowser":
+            return "<svg viewBox=\"0 0 24 24\"><path d=\"M4,6H2V20A2,2 0 0,0 4,22H18V20H4M18,7H15V12.5A2.5,2.5 0 0,1 12.5,15A2.5,2.5 0 0,1 10,12.5A2.5,2.5 0 0,1 12.5,10C13.07,10 13.58,10.19 14,10.5V5H18M20,2H8A2,2 0 0,0 6,4V16A2,2 0 0,0 8,18H20A2,2 0 0,0 22,16V4A2,2 0 0,0 20,2Z\" /></svg>";
+        case "nowPlaying":
+            return "<svg viewBox=\"0 0 24 24\"><path d=\"M19,19H5V5H19M19,3H5A2,2 0 0,0 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V5C21,3.89 20.1,3 19,3M10,8V16L15,12L10,8Z\" /></svg>";
         default:
             break;
     }
